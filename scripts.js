60c4efd5c3e3036495cb0bac8fc485bfa85d4591
Job Chow <jobitoalv@gmail.com>
2023-04-10 15:40:24 +1000

    create full screen mode

--- scripts.js
+++ scripts.js
@@ -33,8 +33,33 @@ function handleProgress() {
 function scrub(e){
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
- 
 }
+
+function enterFullScreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  }else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();     // Firefox
+  }else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();  // Safari
+  }else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();      // IE/Edge
+  }
+};
+
+let btn = document.getElementById("full_screen");
+btn.addEventListener("click", function(){
+  let videoEle = document.querySelector('video');
+  enterFullScreen(videoEle);
+});
+
+document.addEventListener('fullscreenchange', (event) => {
+  if (document.fullscreenElement) {
+    console.log('Entered fullscreen:', document.fullscreenElement);
+  } else {
+    console.log('Exited fullscreen.');
+  }
+});
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
